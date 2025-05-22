b8ad89dc7c64b1dc5b8fc9c8ad73230111a6650f
gabriolho123 <BIBIMARAFANTE@GMAIL.COM>
2025-05-22 13:09:44 -0300

    Conversao_de_Tipos.py

---
+++ Exemplos /Conversao_de_Tipos.py
@@ -0,0 +1,20 @@
+# Conversão de tipos em Python
+
+# int para float
+x = 10
+y = float(x)
+print(x, "->", y, "->", type(y))
+
+# float para int
+z = int(9.8)
+print("Float para int:", z)
+
+# str para int
+numero_str = "25"
+numero_int = int(numero_str)
+print("String para int:", numero_int)
+
+# int para str
+idade = 18
+idade_str = str(idade)
+print("Idade como string:", idade_str)
