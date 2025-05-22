3f9446d0eb486d44328eb135510c7fa985a7ddb3
gabriolho123 <BIBIMARAFANTE@GMAIL.COM>
2025-05-22 12:33:34 -0300

    TiposPrimitivos.py

---
+++ exemplos/TiposPrimitivos.py
@@ -0,0 +1,9 @@
+numero = 10
+decimal = 3.14
+letra = 'G'
+verdadeiro = True
+
+print("Número inteiro:", numero)
+print("Número decimal:", decimal)
+print("Caractere:", letra)
+print("Booleano:", verdadeiro)
