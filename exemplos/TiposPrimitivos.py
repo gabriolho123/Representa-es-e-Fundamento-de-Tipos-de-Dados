59cbea163ee37c02109cba3caa1e3ff5587dfd45
gabriolho123 <BIBIMARAFANTE@GMAIL.COM>
2025-05-22 12:57:20 -0300

    removendo arquivo duplicado

--- exemplos/TiposPrimitivos.py
+++
@@ -1,9 +0,0 @@
-numero = 10
-decimal = 3.14
-letra = 'G'
-verdadeiro = True
-
-print("Número inteiro:", numero)
-print("Número decimal:", decimal)
-print("Caractere:", letra)
-print("Booleano:", verdadeiro)
